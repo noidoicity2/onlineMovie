8b3b94edacdc87b046bf16a06f833d8738882b23
Dat <mynameisdat6@gmail.com>
2021-02-27 22:29:48 +0700

    add blade file

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('admin.layout.mainLayout');
+});
 //Route::get('/getAll', [MovieController::class, 'getAll']);
 
 //start admin route
